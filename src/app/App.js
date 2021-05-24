d645f739b0bc471dbe5058fdedb252800d3429e8
Matthew Saber <mattsaber21@yahoo.com>
2021-05-24 14:03:31 -0700

    updates routes and imports

--- src/app/App.js
+++ src/app/App.js
@@ -2,6 +2,9 @@ import { Component } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Home from '../home/Home';
+import AuthPage from '../auth/AuthPage.js';
+import CocktailPage from '../cocktails/CocktailPage.js';
+import CocktailDetailPage from '../cocktail-detail.js';
 import {
   BrowserRouter as Router,
   Route,
@@ -11,38 +14,69 @@ import {
 import './App.css';
 
 class App extends Component {
+  state = {
+    token: window.localStorage.getItem.apply('TOKEN'),
+    userId: window.localStorage.getItem('USER_ID'),
+    userName: window.localStorage.getItem('USER_NAME')
+  }
+
+  handleUser = user => {
+    window.localStorage.setItem('TOKEN', user.token);
+    window.localStorage.setItem('USER_ID', user.id);
+    window.localStorage.setItem('USER_NAME', user.name);
+    this.setState({ token: user.token });
+  }
 
   render() {
+    const { token, userName } = this.state;
+
     return (
       <div className="App">
         <Router>
-          <Header/>
+          <Header userName={userName} />
           <main>
 
             <Switch>
               <Route path="/" exact={true}
                 render={routerProps => (
-                  <Home {...routerProps}/>
+                  <Home {...routerProps} />
                 )}
               />
 
-              <Route path="/resources" exact={true}
+              <Route path="/auth" exact={true}
                 render={routerProps => (
-                  <div>Implement a page of resources</div>
+                  <AuthPage {...routerProps}
+                    onUser={this.handleUser} />
                 )}
               />
 
-              <Route path="/resources/:id"
+              <Route path="/cocktail" exact={true}
+                render={routerProps => (
+                  token
+                    ? <CocktailPage {...routerProps} />
+                    : <Redirect to="/auth" />
+                )}
+              />
+
+              <Route path="/favorites" exact={true}
+                render={routerProps => (
+                  token
+                    ? <FavoritesPage {...routerProps} />
+                    : <Redirect to="/auth" />
+                )}
+              />
+
+              {/* <Route path="/favorites/:id"
                 render={routerProps => (
                   <div>Implement a page for id {routerProps.match.params.id}</div>
                 )}
-              />
+              /> */}
 
               <Redirect to="/" />
 
             </Switch>
           </main>
-          <Footer/>
+          <Footer />
         </Router>
       </div>
     );
