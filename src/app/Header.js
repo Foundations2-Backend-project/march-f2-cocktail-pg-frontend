32a39652d6a953f9b0d2333f9a9598a61134ff4e
Matthew Saber <mattsaber21@yahoo.com>
2021-05-25 10:40:09 -0700

    updated header with more NavLink, handleSearch, ect

--- src/app/Header.js
+++ src/app/Header.js
@@ -1,18 +1,41 @@
 import { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import './Header.css';
 
 class Header extends Component {
 
-  render() { 
+  handleLogout = () => {
+    window.localStorage.clear();
+    window.location.reload();
+  }
+
+  render() {
+    //const { userName } = this.props;
+
     return (
       <header className="Header">
+        <img
+          className="logo"
+          alt="AA"
+          src="/purple-cocktail.jpeg"
+        />
 
-        <h1>React App</h1>
-        
+        <h1>Cocktail Page</h1>
+        <nav>
+          <NavLink to='/aut'>SignIn</NavLink>
+          <NavLink to="/" exact={true}>Home</NavLink>
+          <NavLink to="/cocktails">arent these cocktails and not favorites??...My Favorites</NavLink>
+          {userName
+            ? <span>Hi {userName}!</span>
+            : <NavLink to="/auth">My Account</NavLink>
+          }
+          <NavLink to='/auth' onClick={this.handleLogout}>LogOut</NavLink>
+        </nav>
+
       </header>
     );
   }
 
 }
- 
+
 export default Header;
