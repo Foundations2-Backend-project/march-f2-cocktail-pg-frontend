95a4407defee997f2cde7f8f438ad91c444e1b9c
AngelDOrtiz <aortizghs@gmail.com>
2021-05-25 10:43:17 -0700

    commit

---
+++ src/cocktails/CocktailPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CocktailPage extends Component {
+    render() {
+        return (
+            <div>
+                <p>Hello</p>
+            </div>
+        );
+    }
+}
